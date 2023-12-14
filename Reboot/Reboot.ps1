1e9b70457db196aa3492d8c4f2fe0fba74a19544
chikaishiro <gran_turismo@126.com>
2023-12-14 16:47:18 +0800

    Update Reboot.ps1

--- Reboot/Reboot.ps1
+++ Reboot/Reboot.ps1
@@ -1,14 +1,1 @@
-$path = "~/flag"
-
-if(!(Test-Path $path))
-{
-    "content" | Out-File -FilePath $path
-    Write-Output "Reboot started"
-    Restart-Computer -Force
-}
-else
-{
-    Write-Output "Reboot finished"
-    Write-Output "Removing flag"
-    Remove-Item $path
-}
+Write-Output("The vm is going to reboot")
